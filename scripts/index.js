0107357360a9f213172780086ecb98038624ac08
Alex <usov.lesha2017@gmail.com>
2023-02-05 14:18:43 +0300

    temp commit

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+for (let elm of elements) {
+  observer.observe(elm);
+}
+
+
+
+
+
+function toggleClass(elem) {
+  if(elem.classList.contains('active')){
+    elem.classList.remove('active');
+  }else{
+    elem.classList.add('active');
+  }
+}
